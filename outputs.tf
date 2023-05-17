d17aa3a2a2368c22a2a6b4ce1e0a331011e37df3
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-05-17 13:54:07 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "rg" {
   value = "The name of the RG is ${var.rgname}"
 }
 
+//je n'ai pas réussi à faire les outputs :(
 /* output "IP" {
     description = "The IP-addresses are"
     value = "The IP-address for the VMs: ${azurerm_network_interface.app_interface2.*.ip_address}"
