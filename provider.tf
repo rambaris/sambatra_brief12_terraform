814a836b7f161a642cbe793bd8054e56839d2af6
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-05-17 13:57:03 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,9 @@
-# Configure the Microsoft Azure Provider
+# Configuration of the Microsoft Azure Provider 
+# J'ai modifié les codes pour plus de sécurité car c'est un répo publique
 provider "azurerm" {
   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
   client_id       = "4adbf51f-2f82-4557-949c-37b1f33d6163"
-  client_secret   = "6fa8Q~RVQ~qxtaFOQbhc4ZfVeMpVACqnRpc5pbN0"
+  client_secret   = "6fa8Q~RVQ~qxtaFOQbhc4ZfVeMpVACqnRpc5pbN012OD"
   tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
   features {}
 }
