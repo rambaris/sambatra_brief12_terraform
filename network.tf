041873da3d1b60180d78e296347d7bb34c83793b
Sambatra RAMANANDRAY <96051607+rambaris@users.noreply.github.com>
2023-05-17 13:55:28 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,15 +1,15 @@
 //Network
 resource "azurerm_virtual_network" "app_network" {
   name                = "app-network"
-  resource_group_name = "${var.rgname}"
-  location            = "${var.location}"
+  resource_group_name = var.rgname
+  location            = var.location
   address_space       = ["10.0.0.0/16"]
 }
 
 //Subnet
 resource "azurerm_subnet" "SubnetA" {
   name                 = "SubnetA"
-  resource_group_name  = "${var.rgname}"
+  resource_group_name  = var.rgname
   virtual_network_name = azurerm_virtual_network.app_network.name
   address_prefixes     = ["10.0.0.0/24"]
   depends_on = [
@@ -20,13 +20,13 @@ resource "azurerm_subnet" "SubnetA" {
 //Network App-VM 1
 resource "azurerm_network_interface" "app_interface1" {
   name                = "app-interface1"
-  location            = "${var.location}"
-  resource_group_name = "${var.rgname}"
+  location            = var.location
+  resource_group_name = var.rgname
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.SubnetA.id
-    private_ip_address_allocation = "Dynamic"    
+    private_ip_address_allocation = "Dynamic"
   }
 
   depends_on = [
@@ -38,13 +38,13 @@ resource "azurerm_network_interface" "app_interface1" {
 //Network App-VM 2
 resource "azurerm_network_interface" "app_interface2" {
   name                = "app-interface2"
-  location            = "${var.location}"
-  resource_group_name = "${var.rgname}"
+  location            = var.location
+  resource_group_name = var.rgname
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.SubnetA.id
-    private_ip_address_allocation = "Dynamic"    
+    private_ip_address_allocation = "Dynamic"
   }
 
   depends_on = [
@@ -56,10 +56,10 @@ resource "azurerm_network_interface" "app_interface2" {
 //Security Group
 resource "azurerm_network_security_group" "app_nsg" {
   name                = "app-nsg"
-  location            = "${var.location}"
-  resource_group_name = "${var.rgname}"
+  location            = var.location
+  resource_group_name = var.rgname
 
-//Security rule for port 80
+  //Security rule for port 80
   security_rule {
     name                       = "Allow_HTTP"
     priority                   = 200
@@ -73,7 +73,7 @@ resource "azurerm_network_security_group" "app_nsg" {
   }
 }
 
-//Security Group
+//Security Group NSG
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   subnet_id                 = azurerm_subnet.SubnetA.id
   network_security_group_id = azurerm_network_security_group.app_nsg.id
